6897a3c21ea253fd25084b1b109e8db2f6c56adf
Warren Buckley <wbuckley@thecogworks.co.uk>
2013-07-25 15:31:26 +0100

    CWS icons from Fugue Icon Set & doctype icons updated

--- CWSStart.Web/CWSStart.Web/CWSExtensions/RegisterStartupEvents.cs
+++ CWSStart.Web/CWSStart.Web/CWSExtensions/RegisterStartupEvents.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using umbraco.cms.businesslogic.member;
+using Umbraco.Core;
 
 namespace CWSStart.Web.CWSExtensions
 {
-    public class RegisterStartupEvents
+    public class RegisterStartupEvents : ApplicationEventHandler
     {
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            //Umbraco App is starting up...
+
+            //Ensure our custom member type & it's properties are setup in Umbraco
+            //If not let's create it
+
+
+            //Create Contact Form Logging DB Table with Umbraco flavoured PetaPoco
+
+        }
+        
     }
 }
