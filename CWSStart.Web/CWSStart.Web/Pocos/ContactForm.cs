f52a6d3f23e5d498fd6016acdfb67162de869f59
Warren Buckley <wbuckley@thecogworks.co.uk>
2013-07-25 17:59:55 +0100

    Start work on Contact Form Controller & View, along with startup event for creating member type automagically

---
+++ CWSStart.Web/CWSStart.Web/Pocos/ContactForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Persistence.DatabaseAnnotations;
+
+namespace CWSStart.Web.Pocos
+{
+    [TableName("ContactFormLogs")]
+    [PrimaryKey("Id", autoIncrement = true)]
+    [ExplicitColumns]
+    public class ContactForm
+    {
+        [Column("id")]
+        [PrimaryKeyColumn(AutoIncrement = true)]
+        public int Id { get; set; }
+
+        [Column("Date")]
+        public DateTime Date { get; set; }
+
+        [Column("Name")]
+        public string Name { get; set; }
+
+        [Column("Email")]
+        public string Email { get; set; }
+
+        [Column("Message")]
+        public string Message { get; set; }
+    }
+}
